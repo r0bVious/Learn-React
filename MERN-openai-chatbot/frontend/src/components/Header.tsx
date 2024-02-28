930d4f846df4868e311684331a50b3e9e7cbd394
r0bVious <robert.spano@gmail.com>
2024-02-28 14:00:53 +0900

    front-end skeleton

---
+++ MERN-openai-chatbot/frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+const Header = () => {
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}></Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
