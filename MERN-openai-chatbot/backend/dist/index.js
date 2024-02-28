fa44ce09ea3ab89ad2abdeedcc1a6bd49413833b
r0bVious <robert.spano@gmail.com>
2024-02-28 12:15:40 +0900

    backend structure

--- MERN-openai-chatbot/backend/dist/index.js
+++ MERN-openai-chatbot/backend/dist/index.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
-
 //connections and listeners
 const PORT = process.env.PORT || 5000;
 connectToDatabase()
-  .then(() => {
+    .then(() => {
     app.listen(PORT, () => console.log("Server Open and Connected to DB"));
-  })
-  .catch((err) => console.error(err));
+})
+    .catch((err) => console.error(err));
+//# sourceMappingURL=index.js.map
---
+++ MERN-openai-chatbot/backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import chatRoutes from "./chat-routes.js";
+import userRoutes from "./user-routes.js";
+
+const appRouter = Router();
+
+appRouter.use("/user", userRoutes); //domain/api/v1/user (see app.ts)
+appRouter.use("/chats", chatRoutes); //domain/api/v1/chats (see app.ts)
+
+export default appRouter;
